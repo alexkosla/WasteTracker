5fdef1507e6d4f5a05b7a7e21dd194dd1386cca9
alexkosla <alexkosla@hotmail.com>
2023-07-10 15:02:46 +0100

    got starter code compiling

---
+++ src/main/java/dcu/ie/WasteTracker/WasteTrackerApplication.java
@@ -0,0 +1,13 @@
+package dcu.ie.WasteTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dcu/ie/WasteTracker/WasteTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dcu.ie.WasteTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
