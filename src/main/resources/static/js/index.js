49d9ae31f30cfba108ebab5c3ab493eb055fa443
alexkosla <alexkosla@hotmail.com>
2023-08-07 16:47:51 +0100

    using promise.all to get data ready for pickup alert calculation

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,86 +1,111 @@
+// import { Calendar } from '@fullcalendar/core';
+
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
-    console.log("hello world dorrrrk");
     
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
         // initialView: 'dayGridWeek',
         height: "90%",
-        themeSystem: 'standard',
+        // themeSystem: 'standard',
         eventTimeFormat: {
             hour: 'numeric',
             minute: '2-digit',
             meridiem: 'short'
-        }
+        }, 
+        headerToolbar: {
+            end: 'dayGridMonth,timeGridWeek today prev,next',
+            start: 'title',
+        },
         // contentHeight: 500
     });
     console.log("about to fetch readings");
     const daily_url = "http://localhost:8080/readings/getDaily";
 
-    var readings = getDailyReadings(calendar);
+    var percents = getDailyReadings(calendar);
     var schedule = getPickupDays(calendar);
-    var changes = getDailyChanges(calendar);
+    var average_changes = getDailyChanges(calendar);
+    Promise.all([average_changes, percents, schedule]).then((values) => {
+        console.log(values);
+        setPickupAlert(values[0], values[1], values[2], calendar);
+    })
 
     // calendar.addEvent({title: '50%', start: '2023-07-17', end: '2023-07-17'});
     // calendar.addEvent({title: '80%', start: '2023-07-18', end: '2023-07-18'});
-    setPickupAlert(readings, schedule, calendar);
+        
+    // setTimeout(() =>
+    // {
+    //     console.log("printing percents after delay");
+    //     console.log(average_changes);
+    //     console.log(percents);
+    //     console.log(schedule);
+    //     setPickupAlert(average_changes, percents, schedule, calendar);
+    // }, 2000);
+    
 
     calendar.render(); 
     console.log(calendar);
 });
 
-function getDailyReadings(calendar)
+async function getDailyReadings(calendar)
 {
-    
-    const daily_url = "http://localhost:8080/readings/getDaily";
-    var readings;
-    fetch(daily_url, {
+    try{
+        const daily_url = "http://localhost:8080/readings/getDaily";
+        var readings;
+        const response = await fetch(daily_url, {
             method: "GET",
             headers: {"Content-type": "application/json"}
-        })
-        .then(response => response.json())
-        .then(data => {
-            readings = data;
-            console.log(data);
-            data.forEach(element => {
-                calendar.addEvent(element)
-            });
-            console.log(calendar.getEvents())
-        })
-        .catch((error) => {
-            window.alert(error);
-        });   
-    return readings;
+        });
+        readings = await response.json();
+        readings.forEach(element => {
+            calendar.addEvent(element)
+        });
+        return readings;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-function getPickupDays(calendar)
+async function getPickupDays(calendar)
 {
-    const users_url = "http://localhost:8080/users/getAll";
-    var schedule;
-
-    fetch(users_url, {
-        method: "GET",
-        headers: {"Content-type": "application/json"}
-    })
-    .then(response => response.json())
-    .then(data => {
-        schedule = data;
-        console.log(data);
-
-        var user = data[0];
+    try{
+        const users_url = "http://localhost:8080/users/getAll";
+        const response = await fetch(users_url, {
+            method: "GET",
+            headers: {"Content-type": "application/json"}
+        });
+        const schedule = await response.json();
+        var user = schedule[0];
 
         if(user != null)
         {
             setPickupDays(user, calendar);
         }
-    })
-    .catch((error) => {
-        window.alert(error);
-    });
-    return schedule;
+        return user;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-function getDailyChanges(calendar)
+async function getDailyChanges(calendar)
+{
+    try{
+        const avg_changes_url = "http://localhost:8080/readings/getDailyChanges";
+        var changes;
+        const response = await fetch(avg_changes_url, {
+            method: "GET",
+            headers: {"Content-type": "application/json"}
+        });
+        changes = await response.json();
+        setAvgChanges(changes, calendar);
+
+        return changes;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function getDailyChangesOld(calendar)
 {
     const avg_changes_url = "http://localhost:8080/readings/getDailyChanges";
     var changes;
@@ -91,6 +116,7 @@ function getDailyChanges(calendar)
         .then(response => response.json())
         .then(data => {
             changes = data;
+            console.log("changes are:");
             console.log(changes);
             setAvgChanges(changes, calendar);
         })
@@ -113,37 +139,54 @@ function setPickupDays(user, calendar)
 
 function setAvgChanges(changes, calendar)
 {
-    if(changes.mondayChange) {calendar.addEvent({title: changes.mondayChange, display:'background', daysOfWeek: [1]})}
-    if(changes.tuesdayChange) {calendar.addEvent({title: changes.tuesdayChange, display: 'block', daysOfWeek: [2]})}
-    if(changes.wednesdayChange) {calendar.addEvent({title: changes.wednesdayChange, display: 'list-item', daysOfWeek: [3]})}
-    if(changes.thursdayChange) {calendar.addEvent({title: changes.thursdayChange, display: 'inverse-background', daysOfWeek: [4]})}
-    if(changes.fridayChange) {calendar.addEvent({title: changes.fridayChange, daysOfWeek: [5]})}
-    if(changes.saturdayChange) {calendar.addEvent({title: changes.saturdayChange, daysOfWeek: [6]})}
-    if(changes.sundayChange) {calendar.addEvent({title: changes.sundayChange, daysOfWeek: [7]})}
+    console.log("in setAvgChanges");
+    if(changes.mondayChange) {calendar.addEvent({title: changes.mondayChange + '% avg increase', color:'rgb(50,50,50)', daysOfWeek: [1]})}
+    if(changes.tuesdayChange) {calendar.addEvent({title: changes.tuesdayChange + '% avg increase', color:'rgb(50,50,50)', daysOfWeek: [2]})}
+    if(changes.wednesdayChange) {calendar.addEvent({title: changes.wednesdayChange + '% avg increase', color:'rgb(50,50,50)', daysOfWeek: [3]})}
+    if(changes.thursdayChange) {calendar.addEvent({title: changes.thursdayChange + '% avg increase', color:'rgb(50,50,50)',  daysOfWeek: [4]})}
+    if(changes.fridayChange) {calendar.addEvent({title: changes.fridayChange + '% avg increase', color:'rgb(50,50,50)', daysOfWeek: [5]})}
+    if(changes.saturdayChange) {calendar.addEvent({title: changes.saturdayChange + '% avg increase', color:'rgb(50,50,50)', daysOfWeek: [6]})}
+    if(changes.sundayChange) {calendar.addEvent({title: changes.sundayChange + '% avg increase', color:'rgb(50,50,50)', daysOfWeek: [0]})}
 }
 
-function setPickupAlert(readings, schedule, calendar)
+/**
+ * 
+ * @param {Array} average_changes 
+ * @param {Array} percents 
+ * @param {Array} schedule 
+ * @param {Calendar} calendar 
+ */
+function setPickupAlert(average_changes, percents, schedule, calendar)
 {
     // using data i've already fetched, figure out if the user should take out their waste
     // display it somehow, probably above the calendar as like an h1
 
-    // fetch(users_url, {
-    //     method: "GET",
-    //     headers: {"Content-type": "application/json"}
-    // })
-    // .then(response => response.json())
-    // .then(data => {
-    //     schedule = data;
-    //     console.log(data);
-    //     console.log(data[0]);
-    //     var user = data[0];
+    var currDate = calendar.getDate();
+    console.log("in setPickupAlert");
+    console.log(average_changes);
+    console.log(percents);
+    console.log(schedule);
 
-    //     if(user != null)
-    //     {
-    //         setPickupDays(user, calendar);
-    //     }
-    // })
-    // .catch((error) => {
-    //     window.alert(error);
-    // });
+    // 1. get the current day
+    // 2. figure out what the next pickup day is
+    // 3. for each of the days in between, get the average changes
+    // 4. add those averages up together with the current fullness
+    // 5. if the sum is greater than 90%, change the text of an H1 alert somewhere on the page
+
+    // 1. get the current day
+    // let's assume that we'll have a reading for today
+    var currReading = percents[percents.length - 1];
+    var currReadingDate = new Date(currReading["end"]);
+    console.log(currReadingDate);
+
+    
+    // 2. figure out what the next pickup day is
+    // add on a date and check if the new date is a pickup day
+    // gets day of the week as a number
+    console.log(currReadingDate.getDay());
+}
+
+function calculateNextPickup(average_changes, percents, schedule)
+{
+    // first calculate if the user needs
 }
